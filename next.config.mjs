5ea8d1c4e5b78526198efc4e7b06bec437b2186d
Ashwin Rajakannan <aswinraja98@gmail.com>
2025-10-29 23:10:10 +0530

    Fix Next.js static export image config for Unsplash

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,14 @@
-/** @type {import('next').NextConfig} */
-
 const nextConfig = {
-  reactStrictMode: true,
   output: 'export',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-    ],
-  },
+        hostname: 'images.unsplash.com'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
